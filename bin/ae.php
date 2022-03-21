dd91ccf4a595241d9b74f6d1b1ed9dbc56c50776
Tivins <pub.git@elabo.fr>
2022-03-21 22:22:32 +0100

    various changes (building/searching)

--- bin/ae.php
+++ bin/ae.php
@@ -2,6 +2,7 @@
 <?php
 
 use Tivins\Core\System\FileSys;
+use Tivins\Core\System\Terminal;
 
 /*
  * The script is wrapped by composer, so autoloader is already on.
@@ -25,7 +26,7 @@ if (!isset($argv[0])) {
 $root = getcwd();
 echo "Current working dir is '$root'." . PHP_EOL;
 
-echo \Tivins\Core\System\Terminal::decorateInfo("Create directories") . PHP_EOL;
+echo Terminal::decorateInfo("Create directories") . PHP_EOL;
 FileSys::mkdir("$root/bin");
 FileSys::mkdir("$root/src");
 FileSys::mkdir("$root/files/cache");
@@ -33,21 +34,36 @@ FileSys::mkdir("$root/templates");
 FileSys::mkdir("$root/public");
 FileSys::mkdir("$root/settings");
 
-echo \Tivins\Core\System\Terminal::decorateInfo('Add to .ignore') . PHP_EOL;
-file_put_contents("$root/.gitignore",
-    "/vendor/\n"
+$gitIgnore = "/vendor/\n"
     . "/settings/*.php\n"
-    . "/files/cache\n"
-    , FILE_APPEND
-);
+    . "/files/cache\n";
 
-$body = '<?php' . "\n\n";
-$body .= 'require __dir__ . \'/../vendor/autoload.php\';' . "\n\n";
-$body .= 'Tivins\AppEngine\Boot::init(__dir__ . \'/..\');' . "\n\n";
-FileSys::writeFile("$root/public/index.php", $body);
-echo \Tivins\Core\System\Terminal::decorateInfo('public/index.php created') . PHP_EOL;
+$index = '<?php' . "\n\n";
+$index .= 'require __dir__ . \'/../vendor/autoload.php\';' . "\n\n";
+$index .= 'Tivins\AppEngine\Boot::init(__dir__ . \'/..\');' . "\n\n";
 
+
+createFileAndGitAdd("$root/.gitignore", $gitIgnore, true);
+createFileAndGitAdd("$root/public/index.php", $index);
 copy(__dir__ . '/../files/models/default.settings.php', "$root/settings/localhost.settings.php");
+copyFileAndGitAdd(__dir__ . '/../files/models/html.html', "$root/templates/html.html");
+copyFileAndGitAdd(__dir__ . '/../files/models/page_maintenance.html', "$root/templates/page_maintenance.html");
 
-copy(__dir__ . '/../files/models/html.html', "$root/templates/html.html");
-copy(__dir__ . '/../files/models/page_maintenance.html', "$root/templates/page_maintenance.html");
+
+function createFileAndGitAdd($dst, $content, bool $append = false)
+{
+    FileSys::writeFile($dst, $content, $append);
+    echo Terminal::decorateInfo("$dst created.") . PHP_EOL;
+    gitAdd($dst);
+}
+function copyFileAndGitAdd($src,$dst)
+{
+    copy($src, $dst);
+    echo Terminal::decorateInfo("$dst created.") . PHP_EOL;
+    gitAdd($dst);
+}
+function gitAdd($file)
+{
+    shell_exec("git add $file");
+    echo Terminal::decorateInfo("$file added to git.") . PHP_EOL;
+}
